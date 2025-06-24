eef3ceec1157ef6fbe64c6038e5389095785f627
g700513 <mohamed.zerai@sagemcom.com>
2025-06-24 10:15:41 +0100

    update aesd-assignments SHA1

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = main
+AESD_ASSIGNMENTS_VERSION = 7e39454f7e67f5b747796de9d0c674da7552dccd
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
